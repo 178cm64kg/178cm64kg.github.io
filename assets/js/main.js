d450609a12ddbe16831038eccac527119b474614
178cm64kg <tanaka.tkmm@gmail.com>
2025-03-02 21:57:43 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
-(function(){
-const navBtn = document.querySelector('.js-nav-btn');
-const navContent = document.querySelector('.js-nav-content');
+const $navBtn = document.querySelector('.js-nav-btn');
+const $navContent = document.querySelector('.js-nav-content');
 
-navBtn.addEventListener('click', function () {
-  navBtn.classList.toggle('is-open');
-  navContent.classList.toggle('is-open');
+$navBtn.addEventListener('click', function () {
+  $navBtn.classList.toggle('is-open');
+  $navContent.classList.toggle('is-open');
 });
-}());
