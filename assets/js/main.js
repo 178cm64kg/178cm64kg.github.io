fe40c7162c139f61aba82baa18278db592d9bdc6
178cm64kg <tanaka.tkmm@gmail.com>
2025-03-02 20:10:11 +0900

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const $navBtn = document.querySelector('.js-nav-btn');
+const $navContent= document.querySelector('.js-nav-content');
+
+$navBtn.addEventListener('click', function () {
+  $navBtn.classList.toggle('is-open');
+  $navContent.classList.toggle('is-open');
+});
